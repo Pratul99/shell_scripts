d19a7dcc0026c35e1bd0cad26c5dce6ec0d4bc89
Pratul Bhatt <117703924+Pratul99@users.noreply.github.com>
2023-01-12 16:57:42 +0530

    Update p2.sh

--- p2.sh
+++ p2.sh
@@ -1,7 +1,43 @@
 #! /usr/bin/bash
 
-for i in $(find . -type d) ;
+rm -r hot
+
+mkdir ./hot
+
+for i in $(find . -type d);
 do
-  each "This is folder :  $i"
+
+if [ "$i" != "./hot"  ]; then
+
+  echo "******* FOLDER $i  *******"
+
+  for myfile in $i/*;
+  do
+    if [ -f "$myfile" ]; then
+       echo "The file inside is $myfile"
+       check=$(grep -ni "while" "$myfile")
+
+      if [ -z "$check" ]; then
+         echo "EMPTY"
+      else
+          echo "HOT FILE   FOUND !!"
+
+          name_fi=$(basename "$myfile")
+
+          cp "$myfile" ./hot/"$name_fi"
+          echo " " >> hot/"$name_fi"
+          echo "*******" >> hot/"$name_fi"
+	      echo "$check" >> hot/"$name_fi"
+
+      fi
+
+    fi
+
+
+  done
+  echo "-----------------"
+  echo " "
+
+fi
 
 done
